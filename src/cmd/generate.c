f87d9c1840d0ef09bbc39eacfea0d7c5fe1c7af0
Chamber6821 <vania.osinin@yandex.ru>
2024-05-20 18:47:48 +0300

    feat(cmd/generate): generate date in MJ

--- src/cmd/generate.c
+++ src/cmd/generate.c
@@ -1,7 +1,10 @@
+#include "julian-time.h"
 #include "structs.h"
+#include "tools.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
@@ -10,10 +13,15 @@ int main(int argc, char *argv[]) {
   }
 
   FILE *file = fopen(argv[1], "w");
-  uint64_t size = 256;
-  fwrite(&size, sizeof(size), 1, file);
-  while (size--) {
-    struct row row = {.date = rand(), .no = rand()};
+  struct index index = {.size = 5};
+  fwrite(&index, sizeof(index), 1, file);
+  double minDate = toMJ(timestamp(1900, 01, 01, 12, 00, 00));
+  double maxDate = toMJ(time(NULL)) - 1;
+  for (uint64_t i = 0; i < index.size; i++) {
+    struct row row = {
+        .date = (maxDate - minDate) * randomProportion() + minDate,
+        .no = rand()
+    };
     fwrite(&row, sizeof(row), 1, file);
   }
   fclose(file);

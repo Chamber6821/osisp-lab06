173df6c26e3b6ef585edc483382dbd9b9676e7a6
Chamber6821 <vania.osinin@yandex.ru>
2024-05-20 23:00:42 +0300

    feat(structs): add row2str

--- src/cmd/show.c
+++ src/cmd/show.c
@@ -1,10 +1,8 @@
-#include "julian-time.h"
 #include "structs.h"
 #include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 int main(int argc, char **argv) {
   if (argc < 2) {
@@ -20,10 +18,7 @@ int main(int argc, char **argv) {
   fclose(file);
   int rowNumberColumns = (int)ceil(log10(index.size));
   for (uint64_t i = 0; i < index.size; i++) {
-    time_t date = fromMJ(rows[i].date);
-    char buffer[] = "1900-01-01 12:00:00";
-    strftime(buffer, sizeof(buffer), "%F %T", gmtime(&date));
-    printf("%*lu: %s #%lu\n", rowNumberColumns, i, buffer, rows[i].no);
+    printf("%*lu: %s\n", rowNumberColumns, i, row2str(&rows[i]));
   }
   free(rows);
 }
--- src/main/structs.h
+++ src/main/structs.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include "julian-time.h"
+#include <limits.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <time.h>
 
 struct row {
   double date;
@@ -11,3 +15,12 @@ struct index {
   uint64_t size;
   struct row rows[];
 };
+
+static const char *row2str(struct row *row) {
+  static char buffer[] = "1900-01-01 12:00:00 #18446744073709551615";
+  time_t time = fromMJ(row->date);
+  char date[] = "1900-01-01 12:00:00";
+  strftime(date, sizeof(date), "%F %T", gmtime(&time));
+  sprintf(buffer, "%s #%lu", date, row->no);
+  return buffer;
+}

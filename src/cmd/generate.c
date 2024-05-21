aef81d56c76ecf6b601a2795ca49985e86a423ff
Chamber6821 <vania.osinin@yandex.ru>
2024-05-21 13:15:30 +0300

    feat(cmd/generate): get row count from cmd arg

--- src/cmd/generate.c
+++ src/cmd/generate.c
@@ -7,13 +7,17 @@
 #include <time.h>
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    fprintf(stderr, "Expected single argument: <filename>\n");
+  if (argc < 3) {
+    fprintf(stderr, "Expected single argument: <filename> <row count>\n");
     return 1;
+  }
+  uint64_t rowCount = 0;
+  if (sscanf(argv[2], "%lu", &rowCount) != 1) {
+    perror("Could not parse row count (2nd arg)\n");
   }
 
   FILE *file = fopen(argv[1], "w");
-  struct index index = {.size = 5};
+  struct index index = {.size = rowCount};
   fwrite(&index, sizeof(index), 1, file);
   double minDate = toMJ(timestamp(1900, 01, 01, 12, 00, 00));
   double maxDate = toMJ(time(NULL)) - 1;
